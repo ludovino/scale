2765fca36538ece84911edd35eb60c36f4f51f5a
Olly L <otcludovino@gmail.com>
2023-11-06 20:58:42 +0000

    initial-commit

---
+++ player.gd
@@ -0,0 +1,89 @@
+extends CharacterBody3D
+
+@export_group("movement")
+@export var speed = 1.5
+@export var fall_acceleration = 75.0
+@export var jump_impulse = 20.0
+@export var speed_size_curve: Curve
+
+@export_group("camera")
+@export var x_invert = true
+@export var x_sensitivity = 1.0
+@export var y_invert = false
+@export var y_sensitivity = 1.0
+@export var high_angle = 60
+@export var low_angle = 60
+
+@export_group("growth")
+@export var shrink_curve: Curve
+@export var shrink_rate = 0.3
+var target_velocity = Vector3.ZERO;
+
+var size_key = 0.5
+var size_factor = 1.0
+var collider_height = 0.0
+var collider_radius = 0.0
+var arm_distance = 0.0
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	collider_height = $Collider.shape.height
+	collider_radius = $Collider.shape.radius
+	arm_distance = $CameraArm.spring_length
+
+
+func _process_shrink(delta):
+	if Input.is_action_pressed("shrink"):
+		size_key = clamp (size_key - shrink_rate * delta * 0.5, 0.0, 1.0)
+		print("shrink")
+	elif Input.is_action_pressed("grow"):
+		size_key = clamp (size_key + shrink_rate * delta * 0.5, 0.0, 1.0)
+		print("grow")
+	else:
+		return
+	size_factor = shrink_curve.sample(size_key)
+	$Collider.shape.height = collider_height * size_factor
+	$Collider.shape.radius = collider_radius * size_factor
+	$CameraArm.spring_length = arm_distance * size_factor
+	$Mesh.scale = Vector3.ONE * size_factor
+
+func _process_movement():
+	var input_velocity = Vector3.ZERO;
+	input_velocity.y = target_velocity.y
+	var cam_rotation = $CameraArm.get_global_transform().basis.get_euler().y
+	if Input.is_action_pressed("move_forward"):
+		input_velocity.z -= 1
+	if Input.is_action_pressed("move_back"):
+		input_velocity.z += 1
+	if Input.is_action_pressed("move_left"):
+		input_velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input_velocity.x += 1
+	target_velocity = input_velocity.rotated(Vector3.UP, cam_rotation)
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		target_velocity.y = jump_impulse
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		var x_change = deg_to_rad(event.relative.x * x_sensitivity)
+		if x_invert:
+			x_change *= -1
+		
+		var y_change = deg_to_rad(event.relative.y * y_sensitivity)
+		if y_invert:
+			y_change *= -1
+			
+		$CameraArm.rotation.y += x_change
+		$CameraArm.rotation.x = clamp($CameraArm.rotation.x + y_change, 
+				-deg_to_rad(low_angle), 
+				deg_to_rad(high_angle))
+
+
+func _physics_process(delta):
+	_process_movement()
+	_process_shrink(delta)
+	
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	
+	velocity = target_velocity * speed * speed_size_curve.sample(size_key)
+	move_and_slide()
